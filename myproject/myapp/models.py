32f31195ca84acbe04ff5bad401685f7296fb14c
jhell85 <jhell85@gmail.com>
2021-07-19 11:51:02 -0700

    made /movie route and model for a movie

---
+++ myproject/myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+# from django_mysql.models import ListCharField
+
+# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot_summary = models.CharField(max_length=1000)
+    release_date = models.DateTimeField(default=timezone.now)
+    director = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
